587d4b9cd81200bc5198273a1ba2e36e6419a120
Chamika-ML <edcdanuruddha@gmail.com>
2024-05-08 23:38:47 +0530

    data ingestion added2

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,7 @@ from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.confing_entity import DataIngestionConfig
 
 class ConfigurationManager:
-    def __init__(self,config_filepath = CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
+    def __init__(self,config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         # create atrifacts folder inside the root folder
